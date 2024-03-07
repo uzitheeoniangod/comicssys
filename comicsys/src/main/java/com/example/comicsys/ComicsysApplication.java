f0853e9eae6d430c05c35eb6771d96bc4571e805
wyl <wylymnl@163.com>
2024-03-07 13:27:35 +0800

    Initial commit

---
+++ comicsys/src/main/java/com/example/comicsys/ComicsysApplication.java
@@ -0,0 +1,13 @@
+package com.example.comicsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComicsysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComicsysApplication.class, args);
+    }
+
+}
---
+++ comicsys/src/test/java/com/example/comicsys/ComicsysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.comicsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComicsysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
